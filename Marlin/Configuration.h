a08b1d2cb7778fca936e8eee9636b020a9599fd4
Jeremy Fairbanks <jsfairbanks@gmail.com>
2025-02-22 21:59:22 -0500

    Enable BLTOUCH flag and set XY limits

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1369,7 +1369,7 @@
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
-//#define BLTOUCH
+#define BLTOUCH
 
 /**
  * MagLev V4 probe by MDD
@@ -1741,8 +1741,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 250
+#define Y_MAX_POS 230
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1033,7 +1033,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define from 3 to 9 points to probe.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 } }
+  #define TRAMMING_POINT_XY { {  30, 30 }, { 100,  30 }, { 100, 100 }, { 30, 100 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
